80c5c0bcbcf98ad3555b6d0183d33aeecae2b095
Labhesh Chaudhari <78224580+labhesh167@users.noreply.github.com>
2025-07-11 15:46:02 +0530

    To Check Number is palindrome or Not

---
+++ ArmStrongNumber.java
@@ -0,0 +1,32 @@
+//Write a Java program to check if the given number is a Armstrong number.
+import java.util.Scanner;
+public class ArmStrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
+        }
+
+        scanner.close();
+    }
+
+    public static boolean isArmstrong(int num) {
+        int originalNum = num;
+        int sum = 0;
+        int digits = String.valueOf(num).length();
+
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, digits);
+            num /= 10;
+        }
+
+        return sum == originalNum;
+    }
+}
